8df5e7fe504bc8ac98479f82d2c2f5199c60671a
Firhan Imam Haekal <45736333+FirH@users.noreply.github.com>
2022-10-21 07:47:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,15 +12,6 @@ str_input = [x for x in str_input if x not in removal]
 for user in usernames :
     if(user in str_input) : 
         out = str_input[str_input.index(user)+1]
-        htmlstr = f"""<p style = 'background-color: rgb(255,255,255,1);
-                        color: rgb(0,0,0,1);'>
-                        {out}</style>
-                        <BR></p>"""
-        st.markdown(htmlstr, unsafe_allow_html=True)
+        st.write(str_input[str_input.index(user)+1])
     else :
-        out = "Nan"
-        htmlstr = f"""<p style = 'background-color: rgb(255,255,255,1);
-                        color: rgb(0,0,0,1);'>
-                        {out}</style>
-                        <BR></p>"""
-        st.markdown(htmlstr, unsafe_allow_html=True)
+        st.write("Nan")
