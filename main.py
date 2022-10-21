dd7f1fd1420bd45b6059943fc46e6add8c9c43f9
Firhan Imam Haekal <45736333+FirH@users.noreply.github.com>
2022-10-21 10:48:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 import streamlit as st
 st.title("""Hackerrank Cleaner""")
 
-CURRENT_THEME = "light"
-IS_DARK_THEME = False
-
 st_input = st.text_input("Masukkan username hackerrank")
 hc_input = st.text_input("Masukkan data hackerrank (CTRL+A) ")
 usernames = st_input.split(" ")
 removal = ['PREPARENEW', 'CERTIFY', 'COMPETE', 'Search', 'All', 'Contests', 'Generic', '2022', 'Leaderboard', 'AllFriendsFilter', 'by', 'Type', 'username', 'to', 'compare', 'Compare', 'Rank', 'User', 'Score', 'Time', 'Interview', 'Prep', '|', 'Blog', '|', 'Scoring', '|', 'Environment', '|', 'FAQ', '|', 'About', 'Us', '|', 'Support', '|', 'Careers', '|', 'Terms', 'Of', 'Service', '|', 'Privacy', 'Policy', '|']
-str_input = hc_input.split(" ")
-str_input = [x for x in str_input if x.strip()]
-str_input = [x for x in str_input if x not in removal]
+str_input = [x for x in [x for x in hc_input.split(" ") if x.strip()] if x not in removal]
 
 for user in usernames :
     if(user in str_input) : 
-        style = f'<p style="font-family:Arial;">{str_input[str_input.index(user)+1]}</p>'
+        style = f'<p style="font-family:Arial; background-color : rgb(255, 255 ,255 1); color : rgb(0,0,0,1)">{str_input[str_input.index(user)+1]}</p>'
         st.markdown(style, unsafe_allow_html=True)
     else :
-        style = '<p style="font-family:Arial; ">Nan</p>'
+        style = '<p style="font-family:Arial; background-color : rgb(255, 255 ,255 1); color : rgb(0,0,0,1); ">Nan</p>'
         st.markdown(style, unsafe_allow_html=True)
