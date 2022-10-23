1fcabafd20cc280b383f21a9630b2735e815cfb5
Firhan Imam Haekal <45736333+FirH@users.noreply.github.com>
2022-10-23 12:14:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import streamlit as st
-st.title("""Hackerrank Cleaner""")
+st.title("""HackerRank Cleaner""")
+st.write(
+"""
+<style>
 
-st_input = st.text_input("Masukkan username hackerrank")
-hc_input = st.text_input("Masukkan data hackerrank (CTRL+A) ")
-usernames = st_input.split(" ")
-removal = ['PREPARENEW', 'CERTIFY', 'COMPETE', 'Search', 'All', 'Contests', 'Generic', '2022', 'Leaderboard', 'AllFriendsFilter', 'by', 'Type', 'username', 'to', 'compare', 'Compare', 'Rank', 'User', 'Score', 'Time', 'Interview', 'Prep', '|', 'Blog', '|', 'Scoring', '|', 'Environment', '|', 'FAQ', '|', 'About', 'Us', '|', 'Support', '|', 'Careers', '|', 'Terms', 'Of', 'Service', '|', 'Privacy', 'Policy', '|']
-str_input = [x for x in [x for x in hc_input.split(" ") if x.strip()] if x not in removal]
+div[class*="stTextInput"] label {
+  font-size: 20px;
+  color: white;
+}
+
+</style>
+""", unsafe_allow_html=True
+)
+
+unused_words = ['PREPARENEW', 'CERTIFY', 'COMPETE', 'Search', 'All', 'Contests', 'Generic', 'Leaderboard', 'AllFriendsFilter', 'by', 'Type', 'username', 'to', 'compare', 'Compare', 'Rank', 'User', 'Score', 'Time', 'Interview', 'Prep', 'Blog', 'Scoring', 'Environment', 'FAQ', 'About', 'Us', 'Support', 'Careers', 'Terms', 'Of', 'Service', 'Privacy', 'Policy', '|']
+usernames = (st.text_input("Masukkan username HackerRank")).split(" ")
+leaderboard_data = [x for x in [x for x in (st.text_input("Masukkan seluruh isi page leaderboard HackerRank (CTRL+A) ")).split(" ") if x.strip()] if x not in unused_words]
 
 for user in usernames :
-    if(user in str_input) : 
-        style = f"""<p style="font-family:Arial; 
+    if(user in leaderboard_data) : 
+        st.markdown(f"""<p style="font-family:Arial; 
                     background-color : white; 
-                    color : black">
-                    {str_input[str_input.index(user)+1]}</style>
-                    </BR></p>"""
-        st.markdown(style, unsafe_allow_html=True)
+                    color : black";>
+                    {leaderboard_data[leaderboard_data.index(user)+1]}</style>
+                    </p>""", unsafe_allow_html=True)
     else :
-        style = """<p style="font-family:Arial; 
+        st.markdown("""<p style="font-family:Arial; 
                     background-color : white; 
                     color : black; ">Nan</style>
-                    </BR></p>"""
-        st.markdown(style, unsafe_allow_html=True)
+                    </p>""", unsafe_allow_html=True)
